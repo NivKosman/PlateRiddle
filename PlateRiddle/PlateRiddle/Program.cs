0d8781c8552e355e850dab51ed8039c814b43864
Niv Kosman <50542585+NivKosman@users.noreply.github.com>
2021-04-07 18:55:32 +0300

    Add files via upload

---
+++ PlateRiddle/PlateRiddle/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlateRiddle
+{
+     class Program
+     {
+          static void Main(string[] args)
+          {
+               Console.WriteLine(String.Format("The minimum attempts of 100-floor building is {0}.",
+                    Calculator.getMinAttemptsToFindMinFloor(100)));//1.1
+
+               Console.WriteLine("please enter a number to find the minimum attempts.(and press after enter)");
+               int inputUser = int.Parse(Console.ReadLine());
+
+               Console.WriteLine(String.Format("The minimum attempts of {0}-floor building is {1}.",
+                    inputUser,Calculator.getMinAttemptsToFindMinFloor(inputUser)));                             
+          }
+     }
+}
